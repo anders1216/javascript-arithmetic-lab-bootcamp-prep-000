be738525d619471b870a579f6d29f9c81842cec2
Anders Hornor <anders.hornor@gmail.com>
2018-11-21 17:55:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return a * B 
+  return a * b 
 }
 
 function divide(a, b) {
   return a / b
 }
+ function inc(n) {
+   return n++
+ }
+ 
+ function dec(n) {
+   return n--
+ }
+ 
+ function makeInt(n){
+   return parseInt(n)
+ }
+ 
+ function 
